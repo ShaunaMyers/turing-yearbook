0b5262483e708f79f313cd66b13c7ac745dd8502
Shauna Myers <shaunamyers26@gmail.com>
2021-07-15 10:15:55 +0100

    Build out basic structure of Person component

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './Person.css';
 
-// PERSON COMPONENT CODE GOES HERE
+const Person = (props) => {
+    return (
+        <div className="personCard">
+            <img>{props.image}</img>
+            <h3>{props.name}</h3>
+            <p>{props.quote}</p>
+            <p>{props.superlative}</p>
+        </div>
+    )
+}
 
 export default Person;
