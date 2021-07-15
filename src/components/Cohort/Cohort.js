6851b7a909f9f8f2a42e6f19fff1948ab629f12f
Shauna Myers <shaunamyers26@gmail.com>
2021-07-15 10:28:41 +0100

    Update Cohort.js so Person cards appear and style background

--- src/components/Cohort/Cohort.js
+++ src/components/Cohort/Cohort.js
@@ -5,13 +5,15 @@ import './Cohort.css';
 
 const Cohort = (props) => {
 
-    const allStaff = props.staff.map(card => {
-        return <Person id={staff.id} name={staff.name} quote={staff.quote} superlative={staff.superlative} photo={staff.photo} />
+    const allStaff = props.staff.map(staff => {
+        return <Person key={staff.id} id={staff.id} name={staff.name} quote={staff.quote} superlative={staff.superlative} photo={staff.photo} />
     })
     return (
         <section>
             <h2>Staff</h2>
+            <div className="staffContainer">
             {allStaff}
+            </div>
         </section>
     )
 }
