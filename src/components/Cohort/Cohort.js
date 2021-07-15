5bd0b59a30468f8c049b7a79f4c68f3288e8af23
Shauna Myers <shaunamyers26@gmail.com>
2021-07-15 10:15:11 +0100

    Build out basic structure of cohort component

--- src/components/Cohort/Cohort.js
+++ src/components/Cohort/Cohort.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { staff } from '../../data/yearbook-data';
 import Person from '../Person/Person';
 import './Cohort.css';
 
-// COHORT COMPONENT CODE GOES HERE
+const Cohort = (props) => {
+
+    const allStaff = props.staff.map(card => {
+        return <Person id={staff.id} name={staff.name} quote={staff.quote} superlative={staff.superlative} photo={staff.photo} />
+    })
+    return (
+        <section>
+            <h2>Staff</h2>
+            {allStaff}
+        </section>
+    )
+}
 
 export default Cohort;
